7c73a1796e0dbcd58c1dee690390d929f2dda9bd
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-05 18:53:42 -0500

    Add files via upload

---
+++ setup/banner.py
@@ -0,0 +1,61 @@
+import os
+import platform
+from setup import colors
+from setup.colors import r,g,y,c , w
+from colorama import Fore , Back
+
+GB = Back.GREEN
+YB = Back.YELLOW
+WB = Back.RED
+
+
+
+logo = f"""
+ ▄█     ▄███████▄                                            
+███    ███    ███                                            
+███▌   ███    ███                                            
+███▌   ███    ███                                            
+███▌ ▀█████████▀                                             
+███    ███                                                   
+███    ███                                                   
+█▀    ▄████▀                                                 
+                                                             
+   ▄████████  ▄██████▄   ▄█    █▄     ▄████████    ▄████████ 
+  ███    ███ ███    ███ ███    ███   ███    ███   ███    ███ 
+  ███    ███ ███    ███ ███    ███   ███    █▀    ███    ███ 
+ ▄███▄▄▄▄██▀ ███    ███ ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀  
+▀▀███▀▀▀▀▀   ███    ███ ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀    
+▀███████████ ███    ███ ███    ███   ███    █▄  ▀███████████ 
+  ███    ███ ███    ███ ███    ███   ███    ███   ███    ███ {Back.RESET}
+  ███    ███  ▀██████▀   ▀██████▀    ██████████   ███    ███ 
+  ███    ███                                      ███    ███ 
+            {y}<{w}/{y}> {c}Author: {w}CPScript {r}|{g}    
+
+{w}<{y}/{w}> {GB}{w}Github : CPScript {Back.RESET}
+{w}<{y}/{w}> {YB}{w}About : This isn't ment for the public...   yet {Back.RESET}                                                                                                    
+"""
+c = colors
+try:
+    from colorama import Fore, Style
+except ModuleNotFoundError:
+    os.system("pip install colorama")
+
+def banner():
+    print(c.ran + logo)
+
+
+
+def banner2():
+
+
+    print(c.ran,"\n" + "|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX, "- " * 4, " [+] Github: https://github.com/CPScript/ ", "- " * 4 + c.ran + "|")
+    print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] About: CPS is a coder that works on Networking, Malware, and Hacks ", "- " * 4+c.ran + "|")
+    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] [+] IP ROUER", "- " * 3+c.ran + "|")
+    print(c.ran + "\n"+ "|" + "*" * 60+c.ran + "|")
+
+def clear():
+    s = platform.platform()
+    if "Windows" in s:
+        os.system("cls")
+    else:
+        os.system("cls") if "Windows" in platform.platform() else os.system("clear")
